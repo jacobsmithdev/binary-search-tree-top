3623438bc46f275e08c9a7e4a2959599ceebc279
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-12-08 09:19:09 -0700

    refactor: Rename Node this.value to this.data
    To match what project instructions use:
    https://www.theodinproject.com/lessons/javascript-binary-search-trees#assignment:~:text=%24%7Bnode.-,data,-%7D%60)

--- Node.js
+++ Node.js
@@ -1,6 +1,6 @@
 export default class Node {
-    constructor(value = null, left = null, right = null) {
-        this.value = value;
+    constructor(data = null, left = null, right = null) {
+        this.data = data;
         this.left = left;
         this.right = right;
     }
