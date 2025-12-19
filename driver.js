f4c50f8d7cba3c5f259a1b5d1f7f00997f9a6fb2
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-12-19 10:54:11 -0700

    feat: Add driver script to project
    As specified by project instructions
    https://www.theodinproject.com/lessons/javascript-binary-search-trees

--- driver.js
+++ driver.js
@@ -1,1 +1,28 @@
-console.log('hello world');
+import Tree from './Tree.js';
+import randomIntArray from './randomIntArray.js';
+
+import prettyPrint from './prettyPrint.js';
+import printElementOrders from './printElementOrders.js';
+
+const arr = randomIntArray(0, 100, 7);
+const tree = new Tree(arr);
+
+console.log(`balanced: ${tree.isBalanced()}`);
+prettyPrint(tree.root);
+printElementOrders(tree);
+
+// Unbalance tree
+tree.insert(101);
+tree.insert(102);
+tree.insert(103);
+
+console.log(`balanced: ${tree.isBalanced()}`);
+prettyPrint(tree.root);
+console.log();
+
+console.log('rebalancing tree...');
+tree.rebalance();
+console.log(`balanced: ${tree.isBalanced()}`);
+prettyPrint(tree.root);
+
+printElementOrders(tree);
