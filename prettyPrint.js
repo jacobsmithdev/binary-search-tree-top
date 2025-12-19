c12b1636bd0d084e13ea14e96015b0c99277666c
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-12-19 10:33:35 -0700

    feat: Add function to print formatted tree to console
    Taken from The Odin Project:
    https://www.theodinproject.com/lessons/javascript-binary-search-trees

---
+++ prettyPrint.js
@@ -0,0 +1,15 @@
+// Taken from The Odin Project:
+// https://www.theodinproject.com/lessons/javascript-binary-search-trees
+
+export default function prettyPrint(node, prefix = '', isLeft = true) {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+}
